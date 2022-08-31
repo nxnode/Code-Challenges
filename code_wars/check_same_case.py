119282dd767e25a94d9215d6dbbbd786caff27b4
Nathan Hefner <nxhefner@gmail.com>
2022-08-31 13:22:20 -0700

    Testing case check from CodeWars

--- code_wars/check_same_case.py
+++ code_wars/check_same_case.py
@@ -1,1 +1,10 @@
-#https://www.codewars.com/kata/5dd462a573ee6d0014ce715b
+# https://www.codewars.com/kata/5dd462a573ee6d0014ce715b
+def same_case(a, b):
+    case_result = 2
+    if isinstance(a, str) == True:
+        if a.isupper() == True and a.isupper() == b.isupper():
+            return case_result == 1
+            if a.isupper() == False and a.isupper() == b.isupper():
+                return case_result == 0
+        else:
+            return case_result == -1
--- code_wars/check_same_case_test.py
+++ code_wars/check_same_case_test.py
@@ -1,1 +1,29 @@
-#https://www.codewars.com/kata/5dd462a573ee6d0014ce715b
+# https://www.codewars.com/kata/5dd462a573ee6d0014ce715b
+# Write a function that will check if two given characters are the same case.
+#
+# If either of the characters is not a letter, return -1
+# If both characters are the same case, return 1
+# If both characters are letters, but not the same case, return 0
+#
+# Examples
+# 'a' and 'g' returns 1
+# 'A' and 'C' returns 1
+# 'b' and 'G' returns 0
+# 'B' and 'g' returns 0
+# '0' and '?' returns -1
+
+from check_same_case import same_case
+
+def test_case():
+    data = (
+        ('C', 'B', 1),
+        ('b', 'a', 1),
+        ('d', 'd', 1),
+        ('A', 's', 0),
+        ('c', 'B', 0),
+        ('b', 'Z', 0),
+        ('\t', 'Z', -1),
+        ('H', ':', -1))
+
+    for (a, b, eq) in data:
+        assert same_case(a, b) == eq
