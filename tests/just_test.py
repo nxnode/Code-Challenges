b43e3bd3937bf7d6d1bd61a80e9d08b43c17a929
Nathan Hefner <nxhefner@gmail.com>
2022-09-01 18:30:48 -0700

    still just cleanup - just deleted files that weren't needed - no change in code, just cleanup

--- tests/just_test.py
+++
@@ -1,8 +0,0 @@
-a_var = 1
-b_var = "B"
-c_var = 'c'
-def example():
-    print(f"a_var= {isinstance(a_var, str)} \n b_var is isupper: {isupper(b_var)} \n c_var isupper: {isupper(c_var)}")
-
-if a_var == 1:
-    example()
