d276ac1042f409f2e2bef41237f6e3c004d61fc8
Nathan Hefner <nxhefner@gmail.com>
2022-09-04 09:37:48 -0700

    error - Please commit your changes or stash them before you switch branches.

---
+++ tests/code_wars/path_test.py
@@ -0,0 +1,6 @@
+import os
+
+import os
+print("Old cwd = " + os.getcwd())
+os.chdir("/home/nxn/github/Code-Challenges")
+print("New cwd = " + os.getcwd())
---
+++ tests/code_wars/subtract_the_sum.py
@@ -0,0 +1,16 @@
+# Example
+#
+# n = 325
+# sum = 3+2+5 = 10
+# n = 325-10 = 315 (not in the list)
+# sum = 3+1+5 = 9
+# n = 315-9 = 306 (not in the list)
+# sum = 3+0+6 = 9
+# n =306-9 = 297 (not in the list)
+def number_minus_its_sum_of_digits in str(starting_number):
+
+    sum_of_digits = 0
+    for starting_number in str(n)
+        sum_of_digits += int(starting_number)
+    starting_number_minus_sum = starting_number - sum_of_digits
+    return starting_number_minus_sum
