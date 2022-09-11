416244c1c281d90ba84b1c78136f89aa1b46560e
Nathan Hefner <nxhefner@gmail.com>
2022-09-11 12:32:05 -0700

    this is what I worked on ALL NIGHT LONG - actual hours

--- code_challenges/code_wars/find_unique_number.py
+++ code_challenges/code_wars/find_unique_number.py
@@ -2,14 +2,21 @@
 
 
 def find_unique_number(numbers):
-    new_numbers = numbers
-    for n in numbers:
-        number_count = 0
-        for y in new_numbers:
-            if n == y:
-                number_count += 1
-        if number_count == 1:
-            return n
+    # new_numbers = numbers
+    # for n in numbers:
+    #     number_count = 0
+    #     for y in new_numbers:
+    #         if n == y:
+    #             number_count += 1
+    #     if number_count == 1:
+    #         return n
+    ## commented out version passed 11 of 12 - failed the big number test
 
-
-## passed 11 of 12 - failed the big number test
+    frequency_of_numbers = {}
+    for single_number in numbers:
+        if single_number in frequency_of_numbers:
+            frequency_of_numbers[single_number] += 1
+        else:
+            frequency_of_numbers[single_number] = 1
+    frequency_of_one = [k for (k, v) in frequency_of_numbers.items() if v == 1]
+    return frequency_of_one[0]
