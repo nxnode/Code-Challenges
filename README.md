cf297c6aeb153909cd1073d32a570f4fa61200d6
Nathan Hefner <nxhefner@gmail.com>
2022-09-12 17:24:05 -0700

    holy mother of if statements! finished simple sentence challenge

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 - **Module:** [code_challenges/code_wars/check_same_case.py](code_challenges/code_wars/check_same_case.py)
 - **Test:** [tests/code_wars/test_check_same_case.py](tests/code_wars/test_check_same_case.py)
 - **URL:** [Check Same Case Challenge](https://www.codewars.com/kata/5dd462a573ee6d0014ce715b)
+
+#### **Simple Sentences** (6kyu)
+- **Module:** [code_challenges/code_wars/simple_sentences.py](code_challenges/code_wars/simple_sentences.py)
+- **Test:** [tests/code_wars/test_simple_sentences.py](tests/code_wars/test_simple_sentences.py)
+- **URL:** [Simple Sentences Challenge](https://www.codewars.com/kata/5297bf69649be865e6000922)
--- code_challenges/code_wars/simple_sentences.py
+++ code_challenges/code_wars/simple_sentences.py
@@ -2,5 +2,28 @@
 
 
 def simple_sentences(words):
+    punctuation = [".", ",", "?", "!"]
+    previous_word = ""
     sentence = ""
+    amount_of_words = len(words)
+    for i, sentence_parts in enumerate(words):
+        if i == 0:
+            sentence += sentence_parts
+            i += 1
+        else:
+            if sentence_parts in punctuation:
+                if sentence_parts == previous_word and i != amount_of_words:
+                    previous_word = sentence_parts
+                    i += 1
+                else:
+                    if i != amount_of_words:
+                        sentence = sentence + sentence_parts
+                        previous_word = sentence_parts
+                        i += 1
+            else:
+                sentence += " " + sentence_parts
+                previous_word = sentence_parts
+                i += 1
+        if i == amount_of_words and previous_word not in punctuation:
+            sentence = sentence + "."
     return sentence
