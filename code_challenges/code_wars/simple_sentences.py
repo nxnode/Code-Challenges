54574a3ccfd0c61f8f2a595f6ec3cff61db278de
Nathan Hefner <nxhefner@gmail.com>
2022-09-14 11:21:13 -0700

    changed nested ifs to elif - still passed

--- code_challenges/code_wars/simple_sentences.py
+++ code_challenges/code_wars/simple_sentences.py
@@ -2,28 +2,29 @@
 
 
 def simple_sentences(words):
-    punctuation = [".", ",", "?", "!"]
     previous_word = ""
     sentence = ""
     amount_of_words = len(words)
-    for i, sentence_parts in enumerate(words):
-        if i == 0:
-            sentence += sentence_parts
-            i += 1
+    count_of_loops = 1
+    for sentence_part in words:
+        if sentence_part == "." and previous_word == ".":
+            count_of_loops += 1
+            previous_word = sentence_part
+        elif sentence_part == ".":
+            sentence = sentence + sentence_part
+            previous_word = sentence_part
+            count_of_loops += 1
+        elif sentence_part == ",":
+            sentence = sentence + sentence_part
+            previous_word = sentence_part
+            count_of_loops += 1
+        elif count_of_loops == amount_of_words and sentence_part != ".":
+            sentence += " " + sentence_part + "."
+        elif count_of_loops == 1:
+            sentence += sentence_part
+            count_of_loops += 1
         else:
-            if sentence_parts in punctuation:
-                if sentence_parts == previous_word and i != amount_of_words:
-                    previous_word = sentence_parts
-                    i += 1
-                else:
-                    if i != amount_of_words:
-                        sentence = sentence + sentence_parts
-                        previous_word = sentence_parts
-                        i += 1
-            else:
-                sentence += " " + sentence_parts
-                previous_word = sentence_parts
-                i += 1
-        if i == amount_of_words and previous_word not in punctuation:
-            sentence = sentence + "."
+            sentence += " " + sentence_part
+            previous_word = sentence_part
+            count_of_loops += 1
     return sentence
