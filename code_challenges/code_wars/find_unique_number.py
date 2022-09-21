b554b1a524d1834fa394a30de42996e6cbdd1da4
Nathan Hefner <nxhefner@gmail.com>
2022-09-21 10:53:01 -0700

    just settings things and format change from black

--- code_challenges/code_wars/find_unique_number.py
+++ code_challenges/code_wars/find_unique_number.py
@@ -2,21 +2,16 @@
 
 
 def find_unique_number(numbers):
-    # new_numbers = numbers
-    # for n in numbers:
-    #     number_count = 0
-    #     for y in new_numbers:
-    #         if n == y:
-    #             number_count += 1
-    #     if number_count == 1:
-    #         return n
-    ## commented out version passed 11 of 12 - failed the big number test
-
     frequency_of_numbers = {}
     for single_number in numbers:
-        if single_number in frequency_of_numbers:
-            frequency_of_numbers[single_number] += 1
-        else:
-            frequency_of_numbers[single_number] = 1
-    frequency_of_one = [k for (k, v) in frequency_of_numbers.items() if v == 1]
+        frequency_of_numbers[single_number] = (
+            frequency_of_numbers.get(single_number, 0) + 1
+        )
+        # if single_number in frequency_of_numbers:
+        #     frequency_of_numbers[single_number] += 1
+        # else:
+        #     frequency_of_numbers[single_number] = 1
+    frequency_of_one = [
+        num for num, count in frequency_of_numbers.items() if count == 1
+    ]
     return frequency_of_one[0]
