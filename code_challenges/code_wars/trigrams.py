b3b945ca28f7f7ccc6974df1cebec1a27565ca9c
Nathan Hefner <nxhefner@gmail.com>
2022-10-03 18:09:36 -0700

    made all of the changes - quite a bit better

--- code_challenges/code_wars/trigrams.py
+++ code_challenges/code_wars/trigrams.py
@@ -2,18 +2,10 @@
 
 
 def extract_trigrams(words):
-    stopping_point = len(words) - 2
     if len(words) <= 3:
         return ""
-    spaces_replaced = ""
-    for i in words:
-        if i == " ":
-            i = "_"
-        spaces_replaced += i
-    words = ""
-    for i in range(len(spaces_replaced)):
-        words += spaces_replaced[i : i + 3] + " "
-        i += 1
-        if i == stopping_point:
-            return words.strip()
-    return words.strip()
+    words = words.replace(" ", "_")
+    trigram = ""
+    for i in range(len(words) - 2):
+        trigram += words[i : i + 3] + " "
+    return trigram.strip()
