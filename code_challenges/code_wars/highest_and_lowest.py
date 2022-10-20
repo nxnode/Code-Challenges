94a1ccf89f9f6b1b12956dcc22363caeac779d78
Nathan Hefner <nxhefner@gmail.com>
2022-10-19 21:12:42 -0700

    Completed Highest_and_Lowest challenge

---
+++ code_challenges/code_wars/highest_and_lowest.py
@@ -0,0 +1,16 @@
+# https://www.codewars.com/kata/554b4ac871d6813a03000035
+
+
+def max_and_min(digits):
+    num_list = [int(num) for num in str(digits).split()]
+    if len(num_list) < 2:
+        return f"{num_list[0]} {num_list[0]}"
+    else:
+        eval_nums = [num_list[0], num_list[1]]
+        i = 0
+    for num in num_list:
+        if num > eval_nums[0]:
+            eval_nums[0] = num
+        if num < eval_nums[1]:
+            eval_nums[1] = num
+    return f"{eval_nums[0]} {eval_nums[1]}"
