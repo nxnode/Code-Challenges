f78c8c2ff01eb313c726b308c3322532862f7477
Nathan Hefner <nxhefner@gmail.com>
2022-10-19 21:16:53 -0700

    corrected it to use a set just for good practice and for when I look back at it in the future

--- code_challenges/code_wars/vowel_count.py
+++ code_challenges/code_wars/vowel_count.py
@@ -2,6 +2,6 @@
 
 
 def vowel_count(sentence):
-    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
     char_count = [char for char in sentence if char in vowels]
     return len(char_count)
