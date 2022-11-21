04aa18c5e0cb97b25ad76b1b0183fb6beca34bb0
Nathan Hefner <nxhefner@gmail.com>
2022-11-21 13:07:00 -0800

    got rid of the if len(duplicate_chars) check for zero

--- code_challenges/code_wars/count_duplicates.py
+++ code_challenges/code_wars/count_duplicates.py
@@ -6,7 +6,4 @@ def count_duplicates(characters):
     for char in characters.lower():
         freq_of_dupes[char] = freq_of_dupes.get(char, 0) + 1
     duplicate_chars = [char for char, count in freq_of_dupes.items() if count > 1]
-    if len(duplicate_chars) == 0:
-        return 0
-
     return len(duplicate_chars)
