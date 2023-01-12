d001b664556a0d9592779c1c97bd795ae0d35700
Nathan Hefner <nxhefner@gmail.com>
2023-01-12 15:31:39 -0800

    finished highest scoring word codewars

--- code_challenges/code_wars/highest_scoring_word.py
+++ code_challenges/code_wars/highest_scoring_word.py
@@ -1,12 +1,43 @@
 # https://www.codewars.com/kata/57eb8fcdf670e99d9b000272/train/python
 
+
 def highest_word(test_string):
-    letter_values = {"a":1
-,"b":2,"c":3,"d":4,"e":5,"f":6,"g":7,"h":8,"i":9,"j":10,"k":11,"l":12,"m":13,"n":14,"o":15,"p":16,"q":17,"r":18,"s":19,"t":20,"u":21,"v":22,"w":23,"x":24,"y":25,"z":26}
-    summed_words = {}
+    letter_values = {
+        "a": 1,
+        "b": 2,
+        "c": 3,
+        "d": 4,
+        "e": 5,
+        "f": 6,
+        "g": 7,
+        "h": 8,
+        "i": 9,
+        "j": 10,
+        "k": 11,
+        "l": 12,
+        "m": 13,
+        "n": 14,
+        "o": 15,
+        "p": 16,
+        "q": 17,
+        "r": 18,
+        "s": 19,
+        "t": 20,
+        "u": 21,
+        "v": 22,
+        "w": 23,
+        "x": 24,
+        "y": 25,
+        "z": 26,
+    }
     word_list = test_string.split(" ")
+    highest_word = ""
+    highest_score = 0
     for word in word_list:
-        summed_words[word] = summed_words.get(word, 0)
+        word_score = 0
         for letter in word:
-            summed_words[word] = int(summed_words[word]) + int(letter_values[letter])
-    return summed_words
+            word_score = word_score + int(letter_values[letter])
+        if word_score > highest_score:
+            highest_word = word
+            highest_score = word_score
+    return highest_word
--- tests/code_wars/test_highest_scoring_word.py
+++ tests/code_wars/test_highest_scoring_word.py
@@ -2,12 +2,13 @@
 
 from code_challenges.code_wars.highest_scoring_word import highest_word
 
+
 def test_highest_word():
-    assert highest_word('man i need a taxi up to ubud') == 'taxi'
-    assert highest_word('what time are we climbing up the volcano') == 'volcano'
-    assert highest_word('take me to semynak') == 'semynak'
-    assert highest_word('aa b') == 'aa'
-    assert highest_word('b aa') == 'b'
-    assert highest_word('bb d') == 'bb'
-    assert highest_word('d bb') == 'd'
+    assert highest_word("man i need a taxi up to ubud") == "taxi"
+    assert highest_word("what time are we climbing up the volcano") == "volcano"
+    assert highest_word("take me to semynak") == "semynak"
+    assert highest_word("aa b") == "aa"
+    assert highest_word("b aa") == "b"
+    assert highest_word("bb d") == "bb"
+    assert highest_word("d bb") == "d"
     assert highest_word("aaa b") == "aaa"
