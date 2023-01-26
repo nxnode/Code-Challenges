b693e5de6f0693dffaefa9584cee6599b85eaffa
Nathan Hefner <nxhefner@gmail.com>
2023-01-26 09:01:40 -0800

    passed all tests and updated readme

--- code_challenges/code_wars/duplicate_encoder.py
+++ code_challenges/code_wars/duplicate_encoder.py
@@ -2,4 +2,15 @@
 
 
 def duplicate_encode(word):
-    return word
+    word = word.lower()
+    freq = {}
+    encoded_word = ""
+    for letter in word:
+        if letter in freq:
+            freq[letter] = ")"
+        else:
+            freq[letter] = "("
+    for letter in word:
+        encoded_word += freq[letter]
+
+    return encoded_word
