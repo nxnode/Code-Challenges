c6c087c1b3c4ddfc5705b4d3b8abe41497c626af
Nathan Hefner <nxhefner@gmail.com>
2023-01-30 16:58:41 -0800

    trying min(), max()

--- code_challenges/code_wars/rgb_to_hex_conversion.py
+++ code_challenges/code_wars/rgb_to_hex_conversion.py
@@ -1,18 +1,28 @@
 # https://www.codewars.com/kata/513e08acc600c94f01000001/train/python
 
 
-def rgb_to_hex(r, g, b):
-    r_hex, g_hex, b_hex = int(r) // 16, int(g) // 16, int(b) // 16
-    hex_ref = {10: "A", 11: "B", 10: "C", 12: "D", 13: "E", 14: "F", 15: "G"}
-    rgb = r, g, b
-    hex = ""
-    for x in rgb:
-        hex += str(x // 16)
-        if x % 16 < 10:
-            hex += str(x % 16)
-        else:
-            hex += str(hex_ref[x])
-    return hex
+def rgb_to_hex(red, green, blue):
+    converted = ""
+    for x in red, green, blue:
+        converted += hex_conversion(int(x))
+    return converted
+
+
+def hex_conversion(color):
+    hex_ref = {10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F"}
+    color_to_hex = ""
+    color = min(color, 0)
+    color = max(color, 255)
+    if color // 16 < 10:
+        color_to_hex = str(color // 16)
+    else:
+        color_to_hex = str(hex_ref[color % 16])
+    if color % 16 < 10:
+        color_to_hex += str(color % 16)
+        return color_to_hex
+    else:
+        color_to_hex += str(hex_ref[color % 16])
+        return color_to_hex
 
 
 if __name__ == "__main__":
