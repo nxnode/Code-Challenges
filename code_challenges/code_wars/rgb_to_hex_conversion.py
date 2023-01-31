9c43985b183035cee5b2fe93b00da288e81c18d9
Nathan Hefner <nxhefner@gmail.com>
2023-01-30 18:38:32 -0800

    passing and code nearly reaching headache inducing level

--- code_challenges/code_wars/rgb_to_hex_conversion.py
+++ code_challenges/code_wars/rgb_to_hex_conversion.py
@@ -1,33 +1,11 @@
 # https://www.codewars.com/kata/513e08acc600c94f01000001/train/python
 
 
-def rgb_to_hex(red, green, blue):
-    converted = ""
-    for x in red, green, blue:
-        converted += hex_conversion(int(x))
-    return converted
-
-
-def hex_conversion(color):
-    hex_ref = {10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F"}
-    color_to_hex = ""
-    color = min(color, 0)
-    color = max(color, 255)
-    if color // 16 < 10:
-        color_to_hex = str(color // 16)
-    else:
-        color_to_hex = str(hex_ref[color % 16])
-    if color % 16 < 10:
-        color_to_hex += str(color % 16)
-        return color_to_hex
-    else:
-        color_to_hex += str(hex_ref[color % 16])
-        return color_to_hex
+def rgb_to_hex(*args):
+    return "{}{}{}".format(
+        *(f"{'' if x > 16 else 0}{x:X}" for x in (min(max(x, 0), 255) for x in args))
+    )
 
 
 if __name__ == "__main__":
-    r, g, b, = (
-        0,
-        0,
-        0,
-    )
+    print(rgb_to_hex(255, 255, 255))
