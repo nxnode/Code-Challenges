9781c8d5c84a4a8b85376b37353bfaaff0a306eb
Nathan Hefner <nxhefner@gmail.com>
2023-01-30 16:00:59 -0800

    pushing changes to work on all computers

--- code_challenges/code_wars/rgb_to_hex_conversion.py
+++ code_challenges/code_wars/rgb_to_hex_conversion.py
@@ -1,5 +1,23 @@
 # https://www.codewars.com/kata/513e08acc600c94f01000001/train/python
 
 
-def rgb_to_hex(rgb):
-    return rgb
+def rgb_to_hex(r, g, b):
+    r_hex, g_hex, b_hex = int(r) // 16, int(g) // 16, int(b) // 16
+    hex_ref = {10: "A", 11: "B", 10: "C", 12: "D", 13: "E", 14: "F", 15: "G"}
+    rgb = r, g, b
+    hex = ""
+    for x in rgb:
+        hex += str(x // 16)
+        if x % 16 < 10:
+            hex += str(x % 16)
+        else:
+            hex += str(hex_ref[x])
+    return hex
+
+
+if __name__ == "__main__":
+    r, g, b, = (
+        0,
+        0,
+        0,
+    )
