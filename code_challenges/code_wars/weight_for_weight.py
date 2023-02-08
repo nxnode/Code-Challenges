ad1eae3c5f4fb0eb4df02af4f89e89737c4d3e6f
Nathan Hefner <nxhefner@gmail.com>
2023-02-07 17:29:24 -0800

    compressed and passed tests

--- code_challenges/code_wars/weight_for_weight.py
+++ code_challenges/code_wars/weight_for_weight.py
@@ -1,25 +1,26 @@
 # https://www.codewars.com/kata/55c6126177c9441a570000cc/train/python
 
 
-def convert_and_sort_weights(wts):
-    weights = wts.split(" ")
-    weights = sorted(weights)
-    weight_dict = {}
-    conv_weights = []
-    sorted_weights = []
-    for weight in weights:
-        added_numbers = 0
-        for number in weight.strip():
-            added_numbers += int(number)
-        conv_weights.append(added_numbers)
-        if added_numbers not in weight_dict:
-            weight_dict[added_numbers] = []
-        weight_dict[added_numbers].append(weight)
+def convert_and_sort_weights(w):
+    return " ".join(sorted(sorted(w.split(" ")), key=lambda n: sum(int(c) for c in n)))
+    # weights = wts.split(" ")
+    # weights = sorted(weights)
+    # weight_dict = {}
+    # conv_weights = []
+    # sorted_weights = []
+    # for weight in weights:
+    #     added_numbers = 0
+    #     for number in weight.strip():
+    #         added_numbers += int(number)
+    #     conv_weights.append(added_numbers)
+    #     if added_numbers not in weight_dict:
+    #         weight_dict[added_numbers] = []
+    #     weight_dict[added_numbers].append(weight)
 
-    conv_weights = sorted(conv_weights)
-    for x in sorted(conv_weights):
-        sorted_weights.append(weight_dict[x])
-    return " ".join(sorted_weights)
+    # conv_weights = sorted(conv_weights)
+    # for x in sorted(conv_weights):
+    #     sorted_weights.append(weight_dict[x])
+    # return " ".join(sorted_weights)
 
 
 if __name__ == "__main__":
